02407928737e5947ed50a1882862ca5b2b50145b
unknown <inaamullah484@gmail.com>
2021-04-11 18:14:38 +0500

    solving error

--- GraphQlSpaceX/server.js
+++ GraphQlSpaceX/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require("./schema");
+const cors = require('cors');
 
 const app = express();
+//Alllow Cross origin 
+app.use(cors());
 
 app.use(
   '/graphql',
